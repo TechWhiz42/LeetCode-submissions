c27b234ea3d206ef20ed661273cc776cfa8d2dcc
Nikhil Kumar <byteburst26@gmail.com>
2026-08-03 22:00:00 +0530

    Time: 37 ms (11.92%), Space: 24.4 MB (17.4%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,6 +1,7 @@
 class Solution {
 private:
-    void bfs(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& grid){
+    void bfs(int row, int col, vector<vector<int>>& vis,
+             vector<vector<char>>& grid) {
         vis[row][col] = 1;
         queue<pair<int, int>> q;
         q.push({row, col});
@@ -8,22 +9,23 @@ private:
         int n = grid.size();
         int m = grid[0].size();
 
-        while(!q.empty()){
+        while (!q.empty()) {
             int row = q.front().first;
             int col = q.front().second;
             q.pop();
 
-            //traverse in the neighbour if it's a land
+            // traverse in the neighbour if it's a land
 
-            int dx[] = {1,-1,0,0};
-            int dy[] = {0,0,-1,1};
+            int dx[] = {1, -1, 0, 0};
+            int dy[] = {0, 0, -1, 1};
 
-            for(int k =0;k<4; k++){
+            for (int k = 0; k < 4; k++) {
                 int nx = row + dx[k];
                 int ny = col + dy[k];
-                if(nx >= 0 && nx < n && ny >= 0 && ny <m && grid[nx][ny] == '1' && vis[nx][ny] == 0){
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m &&
+                    grid[nx][ny] == '1' && vis[nx][ny] == 0) {
                     vis[nx][ny] = 1;
-                    q.push({nx,ny});
+                    q.push({nx, ny});
                 }
             }
         }
@@ -33,17 +35,17 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m,0));
+        vector<vector<int>> vis(n, vector<int>(m, 0));
         int cnt = 0;
-        for(int row = 0; row < n; row++){
-            for (int col = 0;col<m;col++){
-                if(grid[row][col] == '1' && !vis[row][col]){
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                if (grid[row][col] == '1' && !vis[row][col]) {
                     cnt++;
                     bfs(row, col, vis, grid);
                 }
             }
         }
 
-        return cnt; 
+        return cnt;
     }
 };
