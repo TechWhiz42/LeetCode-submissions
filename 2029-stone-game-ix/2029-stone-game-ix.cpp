e5fdf4e730536ead9c1f8d754da92dd7b161b96b
Nikhil Kumar <byteburst26@gmail.com>
2026-08-16 23:02:59 +0530

    Time: 3 ms (75.38%), Space: 131.4 MB (33.5%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.cpp
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -6,12 +6,16 @@ public:
         for (int x : stones)
             ++cnt[x % 3];
 
-        if (cnt[1] == 0 && cnt[2] == 0)
+        int zero = cnt[0];
+        int one = cnt[1];
+        int two = cnt[2];
+
+        if (one == 0 && two == 0)
             return false;
 
-        if (cnt[1] == 0 || cnt[2] == 0)
-            return cnt[1] + cnt[2] > 2 && cnt[0] % 2;
+        if (one == 0 || two == 0)
+            return (one + two > 2 && zero % 2);
 
-        return cnt[0] % 2 == 0 || abs(cnt[1] - cnt[2]) > 2;
+        return zero % 2 ? abs(one - two) > 2 : true;
     }
 };
