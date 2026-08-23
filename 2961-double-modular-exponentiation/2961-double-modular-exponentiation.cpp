33b2808e84b1630e0a94d29c72879896a0e7e443
Nikhil Kumar <byteburst26@gmail.com>
2026-08-23 10:49:12 +0530

    Time: 3 ms (21.76%), Space: 22.5 MB (26.85%) - LeetHub

---
+++ 2961-double-modular-exponentiation/2961-double-modular-exponentiation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int powMod(int x, int n, int M) {
+        long long ans = 1;
+        x %= M;
+        
+        while(n > 0){
+            if(n & 1)
+                ans = (__int128)ans * x% M;
+                
+            x = (__int128)x * x % M;
+            n >>= 1;
+        }
+        return ans;
+    }
+
+    vector<int> getGoodIndices(vector<vector<int>>& variables, int target) {
+        vector<int> ans;
+        for(int i = 0; i< variables.size(); i++){
+            int a = variables[i][0];
+            int b = variables[i][1];
+            int c = variables[i][2];
+            int m = variables[i][3];
+
+            int inner = powMod(a,b,10);
+            int outer = powMod(inner,c,m);
+
+            if(outer == target)
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
